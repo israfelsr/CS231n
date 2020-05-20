302f76823a98ce0bac04fce9cfafff8a7048efe8
israfelsr <israfel.sr@gmail.com>
2020-05-20 18:14:17 +0200

    Add assignment 2 solutions

---
+++ assignment2/README.md
@@ -0,0 +1,38 @@
+## Assigment 2
+
+### Goals
+
+In this assignment you will practice writing backpropagation code, and training Neural Networks and Convolutional Neural Networks. The goals of this assignment are as follows:
+
+- Understand **Neural Networks** and how they are arranged in layered architectures.
+- Understand and be able to implement (vectorized) **backpropagation**.
+- Implement various **update rules** used to optimize Neural Networks.
+- Implement **Batch Normalization** and **Layer Normalization** for training deep networks.
+- Implement **Dropout** to regularize networks.
+- Understand the architecture of **Convolutional Neural Networks** and get practice with training them.
+- Gain experience with a major deep learning framework, such as **TensorFlow** or **PyTorch**.
+
+### Q1: Fully-connected Neural Network (20 points)
+
+The notebook `FullyConnectedNets.ipynb` will introduce you to our modular layer design, and then use those layers to implement fully-connected networks of arbitrary depth. To optimize these models you will implement several popular update rules.
+
+### Q2: Batch Normalization (30 points)
+
+In notebook `BatchNormalization.ipynb` you will implement batch normalization, and use it to train deep fully-connected networks.
+
+### Q3: Dropout (10 points)
+
+The notebook `Dropout.ipynb` will help you implement Dropout and explore its effects on model generalization.
+
+### Q4: Convolutional Networks (30 points)
+
+In the IPython Notebook `ConvolutionalNetworks.ipynb` you will implement several new layers that are commonly used in convolutional networks.
+
+### Q5: PyTorch / TensorFlow on CIFAR-10 (10 points)
+
+For this last part, you will be working in either TensorFlow or PyTorch, two popular and powerful deep learning frameworks. **You only need to complete ONE of these two notebooks.** You do NOT need to do both, and we will *not* be awarding extra credit to those who do.
+
+Open up either `PyTorch.ipynb` or `TensorFlow.ipynb`. There, you will learn how the framework works, culminating in training a convolutional network of your own design on CIFAR-10 to get the best performance you can.
+
+More information [on the assignment webpage](https://cs231n.github.io/assignments2020/assignment2/).
+
---
+++ assignment2/cs231n/classifiers/fc_net.py
@@ -0,0 +1,394 @@
+from builtins import range
+from builtins import object
+import numpy as np
+
+from ..layers import *
+from ..layer_utils import *
+
+
+class TwoLayerNet(object):
+    """
+    A two-layer fully-connected neural network with ReLU nonlinearity and
+    softmax loss that uses a modular layer design. We assume an input dimension
+    of D, a hidden dimension of H, and perform classification over C classes.
+
+    The architecure should be affine - relu - affine - softmax.
+
+    Note that this class does not implement gradient descent; instead, it
+    will interact with a separate Solver object that is responsible for running
+    optimization.
+
+    The learnable parameters of the model are stored in the dictionary
+    self.params that maps parameter names to numpy arrays.
+    """
+
+    def __init__(
+        self,
+        input_dim=3 * 32 * 32,
+        hidden_dim=100,
+        num_classes=10,
+        weight_scale=1e-3,
+        reg=0.0,
+    ):
+        """
+        Initialize a new network.
+
+        Inputs:
+        - input_dim: An integer giving the size of the input
+        - hidden_dim: An integer giving the size of the hidden layer
+        - num_classes: An integer giving the number of classes to classify
+        - weight_scale: Scalar giving the standard deviation for random
+          initialization of the weights.
+        - reg: Scalar giving L2 regularization strength.
+        """
+        self.params = {}
+        self.reg = reg
+
+        ############################################################################
+        # TODO: Initialize the weights and biases of the two-layer net. Weights    #
+        # should be initialized from a Gaussian centered at 0.0 with               #
+        # standard deviation equal to weight_scale, and biases should be           #
+        # initialized to zero. All weights and biases should be stored in the      #
+        # dictionary self.params, with first layer weights                         #
+        # and biases using the keys 'W1' and 'b1' and second layer                 #
+        # weights and biases using the keys 'W2' and 'b2'.                         #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        self.params['W1'] = np.random.normal(scale=weight_scale, size=(input_dim, hidden_dim))
+        self.params['b1'] = np.zeros((hidden_dim,))
+        self.params['W2'] = np.random.normal(scale=weight_scale, size=(hidden_dim, num_classes))
+        self.params['b2'] = np.zeros((num_classes,))
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+    def loss(self, X, y=None):
+        """
+        Compute loss and gradient for a minibatch of data.
+
+        Inputs:
+        - X: Array of input data of shape (N, d_1, ..., d_k)
+        - y: Array of labels, of shape (N,). y[i] gives the label for X[i].
+
+        Returns:
+        If y is None, then run a test-time forward pass of the model and return:
+        - scores: Array of shape (N, C) giving classification scores, where
+          scores[i, c] is the classification score for X[i] and class c.
+
+        If y is not None, then run a training-time forward and backward pass and
+        return a tuple of:
+        - loss: Scalar value giving the loss
+        - grads: Dictionary with the same keys as self.params, mapping parameter
+          names to gradients of the loss with respect to those parameters.
+        """
+        scores = None
+        ############################################################################
+        # TODO: Implement the forward pass for the two-layer net, computing the    #
+        # class scores for X and storing them in the scores variable.              #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        # Extract weights and bias
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        # Forward pass
+        first_layer, cache_first_layer = affine_relu_forward(X, W1, b1)
+        scores, cache_affine_second = affine_forward(first_layer, W2, b2)
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        # If y is None then we are in test mode so just return scores
+        if y is None:
+            return scores
+
+        loss, grads = 0, {}
+        ############################################################################
+        # TODO: Implement the backward pass for the two-layer net. Store the loss  #
+        # in the loss variable and gradients in the grads dictionary. Compute data #
+        # loss using softmax, and make sure that grads[k] holds the gradients for  #
+        # self.params[k]. Don't forget to add L2 regularization!                   #
+        #                                                                          #
+        # NOTE: To ensure that your implementation matches ours and you pass the   #
+        # automated tests, make sure that your L2 regularization includes a factor #
+        # of 0.5 to simplify the expression for the gradient.                      #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        loss, dx = softmax_loss(scores, y)
+        dout_second, grads['W2'], grads['b2'] = affine_backward(dx, cache_affine_second)
+        dx, grads['W1'], grads['b1'] = affine_relu_backward(dout_second, cache_first_layer)
+
+        # Regularization
+        loss += 0.5 * self.reg * (np.sum(W1 * W1) + np.sum(W2 * W2))
+        grads['W2'] += self.reg * W2
+        grads['W1'] += self.reg * W1
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        return loss, grads
+
+
+class FullyConnectedNet(object):
+    """
+    A fully-connected neural network with an arbitrary number of hidden layers,
+    ReLU nonlinearities, and a softmax loss function. This will also implement
+    dropout and batch/layer normalization as options. For a network with L layers,
+    the architecture will be
+
+    {affine - [batch/layer norm] - relu - [dropout]} x (L - 1) - affine - softmax
+
+    where batch/layer normalization and dropout are optional, and the {...} block is
+    repeated L - 1 times.
+
+    Similar to the TwoLayerNet above, learnable parameters are stored in the
+    self.params dictionary and will be learned using the Solver class.
+    """
+
+    def __init__(
+        self,
+        hidden_dims,
+        input_dim=3 * 32 * 32,
+        num_classes=10,
+        dropout=1,
+        normalization=None,
+        reg=0.0,
+        weight_scale=1e-2,
+        dtype=np.float32,
+        seed=None,
+    ):
+        """
+        Initialize a new FullyConnectedNet.
+
+        Inputs:
+        - hidden_dims: A list of integers giving the size of each hidden layer.
+        - input_dim: An integer giving the size of the input.
+        - num_classes: An integer giving the number of classes to classify.
+        - dropout: Scalar between 0 and 1 giving dropout strength. If dropout=1 then
+          the network should not use dropout at all.
+        - normalization: What type of normalization the network should use. Valid values
+          are "batchnorm", "layernorm", or None for no normalization (the default).
+        - reg: Scalar giving L2 regularization strength.
+        - weight_scale: Scalar giving the standard deviation for random
+          initialization of the weights.
+        - dtype: A numpy datatype object; all computations will be performed using
+          this datatype. float32 is faster but less accurate, so you should use
+          float64 for numeric gradient checking.
+        - seed: If not None, then pass this random seed to the dropout layers. This
+          will make the dropout layers deteriminstic so we can gradient check the
+          model.
+        """
+        self.normalization = normalization
+        self.use_dropout = dropout != 1
+        self.reg = reg
+        self.num_layers = 1 + len(hidden_dims)
+        self.dtype = dtype
+        self.params = {}
+
+        ############################################################################
+        # TODO: Initialize the parameters of the network, storing all values in    #
+        # the self.params dictionary. Store weights and biases for the first layer #
+        # in W1 and b1; for the second layer use W2 and b2, etc. Weights should be #
+        # initialized from a normal distribution centered at 0 with standard       #
+        # deviation equal to weight_scale. Biases should be initialized to zero.   #
+        #                                                                          #
+        # When using batch normalization, store scale and shift parameters for the #
+        # first layer in gamma1 and beta1; for the second layer use gamma2 and     #
+        # beta2, etc. Scale parameters should be initialized to ones and shift     #
+        # parameters should be initialized to zeros.                               #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        # Weights and bias initialization
+        Din, Dout = input_dim, hidden_dims[0]
+        for i in range(self.num_layers):
+          self.params['W' + str(i+1)] = np.random.normal(scale=weight_scale, size=(Din, Dout))
+          self.params['b' + str(i+1)] = np.zeros((Dout,))
+          Din = Dout
+          if i < len(hidden_dims) - 1:
+            Dout = hidden_dims[i+1]
+          if i == len(hidden_dims) - 1:
+            Dout = num_classes
+          
+        # BN params initialization es hasta uno menos
+        if self.normalization != None:
+          for i in range(self.num_layers - 1):
+            self.params['gamma' + str(i+1)] = np.ones(shape=(hidden_dims[i]))
+            self.params['beta' + str(i+1)] = np.zeros(shape=(hidden_dims[i]))
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        # When using dropout we need to pass a dropout_param dictionary to each
+        # dropout layer so that the layer knows the dropout probability and the mode
+        # (train / test). You can pass the same dropout_param to each dropout layer.
+        self.dropout_param = {}
+        if self.use_dropout:
+            self.dropout_param = {"mode": "train", "p": dropout}
+            if seed is not None:
+                self.dropout_param["seed"] = seed
+
+        # With batch normalization we need to keep track of running means and
+        # variances, so we need to pass a special bn_param object to each batch
+        # normalization layer. You should pass self.bn_params[0] to the forward pass
+        # of the first batch normalization layer, self.bn_params[1] to the forward
+        # pass of the second batch normalization layer, etc.
+        self.bn_params = []
+        if self.normalization == "batchnorm":
+            self.bn_params = [{"mode": "train"} for i in range(self.num_layers - 1)]
+        if self.normalization == "layernorm":
+            self.bn_params = [{} for i in range(self.num_layers - 1)]
+
+        # Cast all parameters to the correct datatype
+        for k, v in self.params.items():
+            self.params[k] = v.astype(dtype)
+
+    def loss(self, X, y=None):
+        """
+        Compute loss and gradient for the fully-connected net.
+
+        Input / output: Same as TwoLayerNet above.
+        """
+        X = X.astype(self.dtype)
+        mode = "test" if y is None else "train"
+
+        # Set train/test mode for batchnorm params and dropout param since they
+        # behave differently during training and testing.
+        if self.use_dropout:
+            self.dropout_param["mode"] = mode
+        if self.normalization == "batchnorm":
+            for bn_param in self.bn_params:
+                bn_param["mode"] = mode
+        scores = None
+        ############################################################################
+        # TODO: Implement the forward pass for the fully-connected net, computing  #
+        # the class scores for X and storing them in the scores variable.          #
+        #                                                                          #
+        # When using dropout, you'll need to pass self.dropout_param to each       #
+        # dropout forward pass.                                                    #
+        #                                                                          #
+        # When using batch normalization, you'll need to pass self.bn_params[0] to #
+        # the forward pass for the first batch normalization layer, pass           #
+        # self.bn_params[1] to the forward pass for the second batch normalization #
+        # layer, etc.                                                              #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        # Cache list are variables that keeps the cache for backward pass. 
+        # cache_function[i] gives the cache of the function in the i layer 
+        cache_affine = []
+        cache_bn = []
+        cache_ln = []
+        cache_relu = []
+        cache_dropout = []
+        
+        # Forward Pass
+        out = X
+        for i in range(self.num_layers - 1):
+          # Affine
+          W, b = self.params['W' + str(i+1)], self.params['b' + str(i+1)]
+          out, cache = affine_forward(out, W, b)
+          cache_affine.append(cache)
+          # BN
+          if self.normalization=='batchnorm':
+            gamma, beta = self.params['gamma' + str(i+1)], self. params['beta' + str(i+1)]
+            out, cache = batchnorm_forward(out, gamma, beta, self.bn_params[i])
+            cache_bn.append(cache)
+          if self.normalization=='layernorm':
+            gamma, beta = self.params['gamma' + str(i+1)], self.params['beta' + str(i+1)]
+            out, cache = layernorm_forward(out, gamma, beta, self.bn_params[i])
+            cache_ln.append(cache)
+          # ReLU
+          out, cache =  relu_forward(out)
+          cache_relu.append(cache)
+          # Dropout
+          if self.use_dropout:
+            out, cache = dropout_forward(out, self.dropout_param)
+            cache_dropout.append(cache)
+          # Input update
+          x = out
+        
+        # Last Layer
+        W, b = self.params['W' + str(self.num_layers)], self.params['b' + str(self.num_layers)]
+        scores, cache = affine_forward(x, W, b)
+        cache_affine.append(cache)
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        # If test mode return early
+        if mode == "test":
+            return scores
+
+        loss, grads = 0.0, {}
+        ############################################################################
+        # TODO: Implement the backward pass for the fully-connected net. Store the #
+        # loss in the loss variable and gradients in the grads dictionary. Compute #
+        # data loss using softmax, and make sure that grads[k] holds the gradients #
+        # for self.params[k]. Don't forget to add L2 regularization!               #
+        #                                                                          #
+        # When using batch/layer normalization, you don't need to regularize the scale   #
+        # and shift parameters.                                                    #
+        #                                                                          #
+        # NOTE: To ensure that your implementation matches ours and you pass the   #
+        # automated tests, make sure that your L2 regularization includes a factor #
+        # of 0.5 to simplify the expression for the gradient.                      #
+        ############################################################################
+        # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+        N = X.shape[0]
+
+        weight_name = 'W' + str(self.num_layers) 
+        bias_name = 'b' + str(self.num_layers)
+
+        # Loss calculation
+        loss, dx = softmax_loss(scores, y)
+        # Last layer backwards
+        dout, grads[weight_name], grads[bias_name] = affine_backward(dx, cache_affine.pop())
+        # Last layer regularization
+        loss += 0.5 * self.reg * np.sum(np.square(self.params[weight_name]))
+        #grads[weight_name] /= N
+        grads[weight_name] += self.reg * self.params[weight_name]
+        # Layers: self.num_layer - 1 -> 1
+        i = self.num_layers - 2
+        while i >= 0:
+          # Dropout
+          if self.use_dropout:
+            dout = dropout_backward(dout, cache_dropout.pop())
+          # ReLU
+          dout = relu_backward(dout, cache_relu.pop())
+          # BN
+          if self.normalization=='batchnorm':
+            dout, grads['gamma' + str(i+1)], grads['beta' + str(i+1)] = batchnorm_backward(dout, cache_bn.pop())
+          #LN
+          if self.normalization=='layernorm':
+            dout, grads['gamma' + str(i+1)], grads['beta' + str(i+1)] = layernorm_backward(dout, cache_ln.pop())
+          # Affine
+          weight_name = 'W' + str(i+1) 
+          bias_name = 'b' + str(i+1)
+          dout, grads[weight_name], grads[bias_name] = affine_backward(dout, cache_affine.pop())
+          # Regularization
+          loss += 0.5 * self.reg * np.sum(np.square(self.params[weight_name]))
+          #grads[weight_name] /= N
+          grads[weight_name] += self.reg * self.params[weight_name]
+          i -= 1
+
+        # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        ############################################################################
+        #                             END OF YOUR CODE                             #
+        ############################################################################
+
+        return loss, grads
